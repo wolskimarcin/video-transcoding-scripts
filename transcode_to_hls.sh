7e2d129b21708c2acfacba6d70112c59d679a58d
wolskimarcin <wolski.marcin.it@gmail.com>
2024-05-23 11:53:32 +0200

    Add resolution check and include only appropriate resolutions

--- transcode_to_hls.sh
+++ transcode_to_hls.sh
@@ -1,58 +1,90 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 input_file"
-    exit 1
-fi
+check_input() {
+    if [ $# -eq 0 ]; then
+        echo "Usage: $0 input_file"
+        exit 1
+    fi
 
-INPUT_FILE="$1"
+    INPUT_FILE="$1"
 
-if [ ! -f "$INPUT_FILE" ]; then
-    echo "Error: Input file '$INPUT_FILE' not found."
-    exit 1
-fi
+    if [ ! -f "$INPUT_FILE" ]; then
+        echo "Error: Input file '$INPUT_FILE' not found."
+        exit 1
+    fi
 
-FILE_SIZE=$(stat -c%s "$INPUT_FILE")
-MAX_SIZE=$((500 * 1024 * 1024))
+    FILE_SIZE=$(stat -c%s "$INPUT_FILE")
+    MAX_SIZE=$((500 * 1024 * 1024))
 
-if [ "$FILE_SIZE" -ge "$MAX_SIZE" ]; then
-    echo "Error: Input file size is greater than 500MB."
-    exit 1
-fi
+    if [ "$FILE_SIZE" -ge "$MAX_SIZE" ]; then
+        echo "Error: Input file size is greater than 500MB."
+        exit 1
+    fi
 
-OUTPUT_DIR="${INPUT_FILE%.*}_transcoded"
+    VIDEO_INFO=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "$INPUT_FILE")
+    INPUT_WIDTH=$(echo $VIDEO_INFO | cut -d'x' -f1)
+    INPUT_HEIGHT=$(echo $VIDEO_INFO | cut -d'x' -f2)
+}
 
-mkdir -p "$OUTPUT_DIR"
+setup_output_directory() {
+    OUTPUT_DIR="${INPUT_FILE%.*}_transcoded"
+    mkdir -p "$OUTPUT_DIR"
+}
 
-# Transcoding to HLS with different resolutions
-ffmpeg -i "$INPUT_FILE" \
-  -filter_complex " \
-  [0:v]split=4[v240][v360][v480][v720]; \
-  [v240]scale=w=426:h=240[v240out]; \
-  [v360]scale=w=640:h=360[v360out]; \
-  [v480]scale=w=854:h=480[v480out]; \
-  [v720]scale=w=1280:h=720[v720out]" \
-  -map "[v240out]" -c:v:0 libx264 -b:v:0 800k -g 48 -sc_threshold 0 -hls_time 6 -hls_segment_filename "$OUTPUT_DIR/240p_%03d.ts" -hls_playlist_type vod "$OUTPUT_DIR/240p.m3u8" \
-  -map "[v360out]" -c:v:1 libx264 -b:v:1 1200k -g 48 -sc_threshold 0 -hls_time 6 -hls_segment_filename "$OUTPUT_DIR/360p_%03d.ts" -hls_playlist_type vod "$OUTPUT_DIR/360p.m3u8" \
-  -map "[v480out]" -c:v:2 libx264 -b:v:2 2000k -g 48 -sc_threshold 0 -hls_time 6 -hls_segment_filename "$OUTPUT_DIR/480p_%03d.ts" -hls_playlist_type vod "$OUTPUT_DIR/480p.m3u8" \
-  -map "[v720out]" -c:v:3 libx264 -b:v:3 3000k -g 48 -sc_threshold 0 -hls_time 6 -hls_segment_filename "$OUTPUT_DIR/720p_%03d.ts" -hls_playlist_type vod "$OUTPUT_DIR/720p.m3u8" \
-  -map a:0 -c:a aac -b:a 128k -ac 2 -hls_time 6 -hls_segment_filename "$OUTPUT_DIR/audio_%03d.ts" -hls_playlist_type vod "$OUTPUT_DIR/audio.m3u8"
+add_resolution() {
+    local width=$1
+    local height=$2
+    local bitrate=$3
 
-if [ $? -ne 0 ]; then
-    echo "Error: ffmpeg transcoding failed."
-    exit 1
-fi
+    if [ "$INPUT_HEIGHT" -ge "$height" ]; then
+        FILTER_COMPLEX+="[0:v]scale=w=$width:h=$height[v${height}out]; "
+        MAP_ARGS+="-map [v${height}out] -c:v libx264 -b:v ${bitrate}k -g 48 -sc_threshold 0 -hls_time 6 -hls_playlist_type vod -hls_segment_filename $OUTPUT_DIR/${height}p_%03d.ts -f hls $OUTPUT_DIR/${height}p.m3u8 "
+        MASTER_PLAYLIST+="#EXT-X-STREAM-INF:BANDWIDTH=${bitrate}000,RESOLUTION=${width}x${height}\n${height}p.m3u8\n"
+    fi
+}
 
-# Create the master playlist
-echo "#EXTM3U" > "$OUTPUT_DIR/master.m3u8"
-echo "#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"audio\",NAME=\"English\",DEFAULT=YES,AUTOSELECT=YES,URI=\"audio.m3u8\"" >> "$OUTPUT_DIR/master.m3u8"
-echo "#EXT-X-STREAM-INF:BANDWIDTH=800000,RESOLUTION=426x240" >> "$OUTPUT_DIR/master.m3u8"
-echo "240p.m3u8" >> "$OUTPUT_DIR/master.m3u8"
-echo "#EXT-X-STREAM-INF:BANDWIDTH=1200000,RESOLUTION=640x360" >> "$OUTPUT_DIR/master.m3u8"
-echo "360p.m3u8" >> "$OUTPUT_DIR/master.m3u8"
-echo "#EXT-X-STREAM-INF:BANDWIDTH=2000000,RESOLUTION=854x480" >> "$OUTPUT_DIR/master.m3u8"
-echo "480p.m3u8" >> "$OUTPUT_DIR/master.m3u8"
-echo "#EXT-X-STREAM-INF:BANDWIDTH=3000000,RESOLUTION=1280x720" >> "$OUTPUT_DIR/master.m3u8"
-echo "720p.m3u8" >> "$OUTPUT_DIR/master.m3u8"
+transcode_video() {
+    FFMPEG_CMD="ffmpeg -i \"$INPUT_FILE\" -filter_complex \""
+    if [ "$INPUT_HEIGHT" -ge 720 ]; then
+        FFMPEG_CMD+="[0:v]split=4[v240][v360][v480][v720]; [v240]scale=w=426:h=240[v240out]; [v360]scale=w=640:h=360[v360out]; [v480]scale=w=854:h=480[v480out]; [v720]scale=w=1280:h=720[v720out]\" "
+    elif [ "$INPUT_HEIGHT" -ge 480 ]; then
+        FFMPEG_CMD+="[0:v]split=3[v240][v360][v480]; [v240]scale=w=426:h=240[v240out]; [v360]scale=w=640:h=360[v360out]; [v480]scale=w=854:h=480[v480out]\" "
+    elif [ "$INPUT_HEIGHT" -ge 360 ]; then
+        FFMPEG_CMD+="[0:v]split=2[v240][v360]; [v240]scale=w=426:h=240[v240out]; [v360]scale=w=640:h=360[v360out]\" "
+    elif [ "$INPUT_HEIGHT" -ge 240 ]; then
+        FFMPEG_CMD+="[0:v]split=1[v240]; [v240]scale=w=426:h=240[v240out]\" "
+    fi
 
-echo "Transcoding completed. Output files are stored in: $OUTPUT_DIR"
+    FFMPEG_CMD+="$MAP_ARGS -map a:0 -c:a aac -b:a 128k -ac 2 -hls_time 6 -hls_playlist_type vod -f hls -hls_segment_filename \"$OUTPUT_DIR/audio_%03d.ts\" \"$OUTPUT_DIR/audio.m3u8\""
+    echo "Executing command: $FFMPEG_CMD"
+    eval $FFMPEG_CMD
+
+    if [ $? -ne 0 ]; then
+        echo "Error: ffmpeg transcoding failed."
+        exit 1
+    fi
+}
+
+create_master_playlist() {
+    echo -e "$MASTER_PLAYLIST" > "$OUTPUT_DIR/master.m3u8"
+    echo "Transcoding completed. Output files are stored in: $OUTPUT_DIR"
+}
+
+main() {
+    check_input "$@"
+    setup_output_directory
+
+    FILTER_COMPLEX=""
+    MAP_ARGS=""
+    MASTER_PLAYLIST="#EXTM3U\n#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"audio\",NAME=\"English\",DEFAULT=YES,AUTOSELECT=YES,URI=\"audio.m3u8\"\n"
+
+    add_resolution 426 240 800
+    add_resolution 640 360 1200
+    add_resolution 854 480 2000
+    add_resolution 1280 720 3000
+
+    transcode_video
+    create_master_playlist
+}
+
+main "$@"
